3ee02ba342fbfcdb2b25669cd3e03110f98d6464
think2011 <452125301.hzplay@gmail.com>
2021-07-31 15:02:19 +0800

    feat: ？？？？？？？？？

--- main.js
+++ main.js
@@ -1,5 +1,15 @@
 var child_process = require('child_process')
-var curl = 'curl http://www.baidu.com'
+const fs = require('fs')
+
+var curl = `curl 'https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=693axxx6-7aoc-4bc4-97a0-0ec2sifa5aaa' \
+-H 'Content-Type: application/json' \
+-d '
+{
+     "msgtype": "text",
+     "text": {
+         "content": "${fs.readFileSync('./CHANGELOG.md', 'utf-8')}"
+     }
+}`
 var child = child_process.exec(curl, function(err, stdout, stderr) {
   console.log(stdout)
 })
