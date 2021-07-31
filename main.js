5966a13c8d0323b97382f41fb6164358a1aa4e21
think2011 <452125301.hzplay@gmail.com>
2021-07-31 14:50:54 +0800

    feat: xxxxxxx

--- main.js
+++ main.js
@@ -1,1 +1,5 @@
-console.log('111111111111111111111111111111')
+var child_process = require('child_process')
+var curl = 'curl http://www.baidu.com'
+var child = child_process.exec(curl, function(err, stdout, stderr) {
+  console.log(stdout)
+})
