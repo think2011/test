24b1e11794a1ecda23e699aa085eba4261d74b2d
think2011 <452125301.hzplay@gmail.com>
2021-07-31 15:27:49 +0800

    feat: 这就是新版本

--- main.js
+++ main.js
@@ -1,15 +1,21 @@
 var child_process = require('child_process')
 const fs = require('fs')
+const axios = require('axios')
 
-var curl = `curl 'https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=693axxx6-7aoc-4bc4-97a0-0ec2sifa5aaa' \
--H 'Content-Type: application/json' \
--d '
-{
-     "msgtype": "text",
-     "text": {
-         "content": "${fs.readFileSync('./CHANGELOG.md', 'utf-8')}"
-     }
-}`
-var child = child_process.exec(curl, function(err, stdout, stderr) {
-  console.log(stdout)
-})
+const text = fs.readFileSync('./CHANGELOG.md', 'utf-8')
+
+axios
+  .post(
+    'https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=5c4e69de-4068-483a-a5cd-ed4fb888b22f',
+    {
+      msgtype: 'markdown',
+      markdown: {
+        content: `
+        # 新版本已发布，查看详情 [<font color=\"info\">CHANGELOG.md</font>](https://github.com/think2011/test/blob/master/CHANGELOG.md)
+
+        ${text.substr(0, 300)}...`,
+      },
+    }
+  )
+  .then(console.log)
+  .catch(console.log)
